babc276a6631d4cd643801246240b6e58190241c
Dawid <yujianzegege@163.com>
2019-06-27 05:19:42 -0500

    hello world version2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("hhhh");
         System.out.println("aaaa");
+        System.out.println("ddd");
     }
 }
